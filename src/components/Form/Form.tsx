1b6c623ef16e9ec0e3cc6fda539cd0f4dc8bab9f
Ben Black <benblack7@gmail.com>
2021-11-22 16:49:04 -0600

    Converted Form into a Controlled Form with Hooks

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css';
 
 
 const Form = () => {
+  const[heading, setHeading] = useState('')
+  const[subHeading, setSubHeading] = useState('')
+  const[summary, setSummary] = useState('')
+
+  const onHeadingChange = (e: React.ChangeEvent<HTMLInputElement>) => setHeading(e.target.value)
+  const onSubHeadingChange = (e: React.ChangeEvent<HTMLInputElement>) => setSubHeading(e.target.value)
+  const onSummaryChange = (e: React.ChangeEvent<HTMLTextAreaElement>)=> setSummary(e.target.value)
+
   return (
     <React.Fragment>
       <form id="msform">
         <fieldset>
           <h2>Create Post</h2>
           <h3>Fill the all mention detail to submit the form</h3>
-          <input type="text" name="heading" value={''} placeholder="Heading" />
-          <input type="text" name="pass" value={''} placeholder="Sub-Heading" />
-          <textarea value={''} placeholder="Summary" />
+          <input
+            onChange={onHeadingChange}
+            type="text"
+            name="heading"
+            value={heading}
+            placeholder="Heading"
+            />
+          <input
+          onChange={onSubHeadingChange}
+          type="text"
+          name="pass"
+          value={subHeading}
+          placeholder="Sub-Heading"
+          />
+          <textarea
+          onChange={onSummaryChange}
+          value={summary}
+          placeholder="Summary"
+          />
           <input type="button" name="Submit" value="Submit" />
         </fieldset>
       </form>
